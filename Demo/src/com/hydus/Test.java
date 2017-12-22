7a6012301a2ce680e6fa17c5d65df09f71795311
asahu <asahu@AFHSJAK.HYDUS.COM>
2017-12-22 10:47:50 +0530

    Hello Class

---
+++ Demo/src/com/hydus/Test.java
@@ -0,0 +1,10 @@
+package com.hydus;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("Hello Welcome To Java");
+	}
+
+}
