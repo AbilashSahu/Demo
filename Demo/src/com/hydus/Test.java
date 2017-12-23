1edeed057ebab2c481d1f2b77e897844d3241f45
asahu <asahu@AFHSJAK.HYDUS.COM>
2017-12-23 20:11:09 +0530

    4th

--- Demo/src/com/hydus/Test.java
+++ Demo/src/com/hydus/Test.java
@@ -1,17 +1,18 @@
 package com.hydus;
 
-class A{
+/*class A{
 	void m1(){
 		System.out.println("Java is a programming language");
 	}
-}
+}*/
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("Hello Welcome To Java");
-   A a=new A();
+   /*A a=new A();
+   a.m1();*/
 	}
 
 }
